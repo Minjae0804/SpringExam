971a9091ae95d4fb8a9ac152fcec48b447b11cc5
Minjae <kums200@gmail.com>
2025-07-15 14:25:26 +0900

    샤샷

---
+++ src/main/java/com/minjae/demo/GreetingRequest.java
@@ -0,0 +1,13 @@
+package com.minjae.demo;
+
+public class GreetingRequest {
+    private String name;
+
+    public GreetingRequest() {}
+
+    public GreetingRequest(String name) { this.name = name; }
+
+    public String getName() { return name; }
+
+    public void setName(String name) { this.name = name; }
+}
